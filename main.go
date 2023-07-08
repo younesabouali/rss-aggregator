65353f70ffb4445ce2f42b8ff476110e8f42bbf8
Younes Abouali <younesabouali3@gmail.com>
2023-07-08 14:28:04 +0100

    calling the scrapper

--- main.go
+++ main.go
@@ -3,16 +3,20 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	DbManager "github.com/younesabouali/rss-aggregator/internal"
+	"github.com/younesabouali/rss-aggregator/scrapper"
 )
 
 func main() {
 
 	godotenv.Load()
 	DB := DbManager.Manager()
+	feedScrap := scrapper.FeedScrapper{DB: DB}
+	go feedScrap.Scrape(5, 1*time.Minute)
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("PORT is not found")
